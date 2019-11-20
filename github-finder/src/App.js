6ef5d0ddba4eb18b3e993f0a22041990beb62b05
Alan Corrales <alancorrales31@gmail.com>
2019-11-20 17:13:46 -0300

    Adds navbar

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+      </div>
+    );
+  }
 }
 
 export default App;
