a21c3d06f2658d7c2a254b5941380ecc7f8b5dcd
Alan <a.corrales@sidi.org.br>
2019-11-22 16:01:36 -0300

    Adds type definition to search function

--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
     state = {
         text: ''
+    }
+
+    static propTypes = {
+        serachUsers: PropTypes.func.isRequired
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value });
