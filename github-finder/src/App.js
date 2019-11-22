7d8f60b950380653b0b4c41f5d8fab3ecb51ca9e
Alan <a.corrales@sidi.org.br>
2019-11-22 16:00:19 -0300

    Adds users search feature

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
+import Search from "./components/users/Search";
 
 
 class App extends Component {
@@ -11,12 +12,12 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+  searchUsers = async (query) => {
     this.setState({ loading: true });
-    const res = await axios.get(`http://api.github.com/users?
-    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false });
+    const res = await axios.get(`http://api.github.com/search/users?q=${query}&
+      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false });
   }
 
   render() {
@@ -24,6 +25,7 @@ class App extends Component {
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
